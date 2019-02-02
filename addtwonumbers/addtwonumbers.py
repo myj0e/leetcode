1856509bfd332400cd6f22e0d769f1d6cfdad56d
Hoop <542813407@qq.com>
2019-02-02 22:58:07 +0800

    first half of addTwoNumbers

---
+++ addtwonumbers/addtwonumbers.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        p1=l1
+        p2=l2
+        while(p1 or p2):
+            if(p1 and p2):
+                
