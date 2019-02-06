d801d451beeacdf993b6c025b1a38a605f908bc7
Hoop <542813407@qq.com>
2019-02-06 12:01:25 +0800

    addTwoNumbers solved in python

--- addtwonumbers/addtwonumbers.py
+++ addtwonumbers/addtwonumbers.py
@@ -12,37 +12,16 @@ class Solution:
         :rtype: ListNode
         """
         #init
-        L1=[]
-        L2=[]
-        res=[]
+        res=ListNode(0)
+        p=res
         cf=0
-        n=l1
-        while(n!=None):
-            L1.append(n.val)
-            n=n.next
-        n=l2
-        while(n!=None):
-            L2.append(l2.val)
-            n=n.next
-        Max=0
-        if len(L1)>len(L2):
-            Max=[len(L2),len(L1),1]
-        else:
-            Max=[len(L1),len(L2),2]
-
-        for i in range(Max[1]):
-            if i+1>Max[0]:
-                res.append((L1[i]+L2[i]+cf)%10)
-                cf=(int)((L1[i]+L2[i]+cf)/10)
-            elif Max[2]==1:
-                res.append((L1[i]+cf)%10)
-                cf=(int)((L1[i]+cf)/10)
-            elif Max[2]==2:
-                res.append((L2[i]+cf)%10)
-                cf=(int)((L2[i]+cf)/10)
-        n=ListNode(0)
-        for i in range(len(res))[::-1]:
-            p=ListNode(res[i])
-            n.next,p.next=p,n.next
-        return n.next
-# 报错了。。。
+        while(l1 or l2 or cf):
+            val=((l1 and l1.val or 0)+(l2 and l2.val or 0)+cf)#and与or的整型数应用
+            p.next=ListNode(val%10)
+            cf=(int)(val/10)
+            if l1:
+                l1=l1.next
+            if l2:
+                l2=l2.next
+            p=p.next
+        return res.next
